c3ac5eb3af33a70e7127e3f33ac387528044477f
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-09-25 15:21:40 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,45 +1,24 @@
 import sys
-
-# 정수 X를 스택에 넣는 연산이다.
-def push(x):
-    stack.append(x)
-
-# 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-def pop():
-    if(not stack):
-        return -1
+n = int(sys.stdin.readline())
+stack =[]
+for i in range(n):
+  cmd = sys.stdin.readline().split()
+  if cmd[0] == 'push':
+    stack.append(cmd[1])
+  elif cmd[0] == 'pop':
+    if len(stack) == 0:
+      print(-1)
     else:
-        return stack.pop()
-
-# 스택에 들어있는 정수의 개수를 출력한다.
-def size():
-    return len(stack)
-
-# 스택이 비어있으면 1, 아니면 0을 출력한다.
-def empty():
-    return 0 if stack else 1
-
-# 스택의 가장 위에 있는 정수를 출력한다. 
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-def top():
-    return stack[-1] if stack else -1
-
-N = int(sys.stdin.readline().rstrip())
-stack = []
-
-for _ in range(N):
-    input_split = sys.stdin.readline().rstrip().split()
-
-    order = input_split[0]
-
-    if order == "push":
-        push(input_split[1])
-    elif order == "pop":
-        print(pop())
-    elif order == "size":
-        print(size())
-    elif order == "empty":
-        print(empty())
-    elif order == "top":
-        print(top())
+      print(stack.pop())
+  elif cmd[0] == 'size':
+    print(len(stack))
+  elif cmd[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif cmd[0] == 'top':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack[-1])
