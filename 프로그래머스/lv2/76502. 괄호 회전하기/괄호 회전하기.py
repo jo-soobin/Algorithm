89d564cda7ea393a271bc81842263dc3d0eb663d
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-11-10 20:28:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,28 @@
+def solution(s):
+    answer = 0
+    arr = [s]
+    for i in range(len(s)-1):
+        arr.append(arr[-1][1:]+arr[-1][0])
+    
+    for i in arr:
+        if check(i):
+            answer+=1
+    
+    return answer
+
+def check(s):
+    stack = [s[0]]
+    for i in s[1:]:
+        if len(stack) == 0:
+            stack.append(i)
+        elif i == ')' and stack[-1]=='(':
+            del stack[-1]
+        elif i==']' and stack[-1]=='[':
+            del stack[-1]
+        elif i=='}' and stack[-1]=='{':
+            del stack[-1]
+        else:
+            stack.append(i)
+    if len(stack)==0:
+        return True
+    return False
