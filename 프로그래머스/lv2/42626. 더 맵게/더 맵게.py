f589df6f4fdeca8082626af8238f041ba73c2306
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2023-02-18 14:33:35 +0900

    [level 2] Title: 더 맵게, Time: 1753.18 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,28 +1,15 @@
-from collections import deque
+import heapq
 
 def solution(S, K):
-    answer = 0 
-    mix = deque()
-    S.sort()
-    sco = deque(i for i in S)
-    
-    while (sco and sco[0] < K) or (mix and mix[0] < K):
-        answer += 1
-        if len(sco) + len(mix) <= 1:
+    heap = []
+    for i in S:
+        heapq.heappush(heap, i)
+
+    cnt = 0
+    while heap[0] < K:
+        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
+        cnt += 1
+        
+        if len(heap) == 1 and heap[0] < K:
             return -1
-        
-        food = [0]*2
-        for a in range(2):
-            if sco and mix:
-                if sco[0] < mix[0]:
-                    food[a] = sco.popleft()
-                else:
-                    food[a] = mix.popleft()
-            elif sco:
-                food[a] = sco.popleft()
-            else:
-                food[a] = mix.popleft()
-            
-        mix.append(food[0]+food[1]*2)
-        
-    return answer
+    return cnt
