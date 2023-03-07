07f789abfa28c199d9265602c3585dfc8cb8144b
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2023-03-07 22:57:29 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,10 +1,9 @@
 def solution(n):
+    num = n**0.5
     answer = 0
-    num = n ** 0.5
-
+    
     if num == int(num):
-        answer = (num+1) ** 2
+        answer = (num+1)**2
     else:
         answer = -1
-
     return answer
