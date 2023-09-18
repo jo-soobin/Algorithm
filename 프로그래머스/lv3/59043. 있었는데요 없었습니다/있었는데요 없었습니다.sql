4ace375f0a4f85a7a676ab8744e18f21305a5fc2
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2023-09-18 22:00:47 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
 SELECT a.animal_id, a.name
-from animal_ins A, animal_outs B
+from animal_ins A
+JOIN animal_outs B
+ON A.ANIMAL_ID = B.ANIMAL_ID
 where a.animal_id = b.animal_id and a.datetime >= b.datetime
 order by a.datetime asc
